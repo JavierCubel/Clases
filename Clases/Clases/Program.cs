eb5dec8984f30166c46d3b98791ec084ed7966bd
Javier Cubel <javicalamocha91@hotmail.es>
2017-09-02 14:18:23 +0200

    2/9/17 14:18

---
+++ Clases/Clases/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Clases
+{
+    /// <summary>
+    /// Clases
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rect = new Rectangulo();
+            rect.Base = 10;
+            rect.Altura = 5;
+
+            int area = rect.Area();
+            int per = rect.Perimetro();
+            double diag = rect.Diagonal();
+            Console.WriteLine("rect: {0} x {1} su area es {2}", rect.Base, rect.Altura, area);
+            Console.WriteLine("rect: {0} x {1} su perimetro es {2}", rect.Base, rect.Altura, per);
+            Console.WriteLine("rect: {0} x {1} su diagonal es {2}", rect.Base, rect.Altura, diag);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Clases/Clases/Rectangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Clases
+{
+    public class Rectangulo
+    {
+        public int Base {get; set; }
+        public int Altura { get; set; }
+
+        public int Area()
+        {
+            return Base * Altura;
+        }
+        public int Perimetro()
+        {
+            return 2*(Base + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Base * Base + Altura * Altura);
+        }
+    }
+}
