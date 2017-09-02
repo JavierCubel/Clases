f0e16966895d5aa21a909aa7b57b39877ce49d03
Javier Cubel <javicalamocha91@hotmail.es>
2017-09-02 14:23:48 +0200

    2/9/17 14:23

--- Clases/Clases/Program.cs
+++ Clases/Clases/Program.cs
@@ -20,8 +20,18 @@ namespace Clases
             Console.WriteLine("rect: {0} x {1} su area es {2}", rect.Base, rect.Altura, area);
             Console.WriteLine("rect: {0} x {1} su perimetro es {2}", rect.Base, rect.Altura, per);
             Console.WriteLine("rect: {0} x {1} su diagonal es {2}", rect.Base, rect.Altura, diag);
-            Console.ReadKey();
+            
 
+            Rectangulo r2 = new Rectangulo {Base=3, Altura=8 };
+
+            int a2 = r2.Area();
+            int p2 = r2.Perimetro();
+            double d2 = r2.Diagonal();
+            Console.WriteLine("r2: {0} x {1} su area es {2}", r2.Base, r2.Altura, a2);
+            Console.WriteLine("r2: {0} x {1} su perimetro es {2}", r2.Base, r2.Altura, p2);
+            Console.WriteLine("r2: {0} x {1} su diagonal es {2}", r2.Base, r2.Altura, d2);
+
+            Console.ReadKey();
 
         }
     }
